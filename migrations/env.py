e421ea190e1bf8a3a04a3db2fde333eddb2e0161
Chaban Sergey <loriston@rambler.ru>
2024-01-24 16:12:26 +0300

    the unique constraint for a Response table has been added

--- migrations/env.py
+++ migrations/env.py
@@ -6,8 +6,9 @@ from sqlalchemy import engine_from_config, pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import AsyncEngine
 
-from settings.database import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER, Base
-from src.tables import *
+from src.database.engine import Base
+from src.database.tables import *
+from src.settings.database import DataBaseSettings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -15,11 +16,13 @@ config = context.config
 
 section = config.config_ini_section
 
-config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section, "DB_PASS", DB_PASS)
-config.set_section_option(section, "DB_NAME", DB_NAME)
-config.set_section_option(section, "DB_PORT", DB_PORT)
+database_settings = DataBaseSettings()
+
+config.set_section_option(section, "DB_USER", database_settings.user)
+config.set_section_option(section, "DB_HOST", database_settings.host)
+config.set_section_option(section, "DB_PASS", database_settings.password)
+config.set_section_option(section, "DB_NAME", database_settings.name)
+config.set_section_option(section, "DB_PORT", str(database_settings.port))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- src/database/tables/responses.py
+++ src/database/tables/responses.py
@@ -1,14 +1,17 @@
-from src.database.engine import Base
 import sqlalchemy as sa
 from sqlalchemy.orm import relationship
+
+from src.database.engine import Base
 
 
 class Response(Base):
     __tablename__ = "responses"
+    # задаем ограничение, чтобы кандидат не мог дважды откликнуться на одну и ту же вакансию
+    __table_args__ = (sa.UniqueConstraint("user_id", "job_id", name="response_unique_key"),)
 
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, comment="Идентификатор отклика")
-    user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'), comment="Идентификатор пользователя", nullable=False)
-    job_id = sa.Column(sa.Integer, sa.ForeignKey('jobs.id'), comment="Идентификатор вакансии", nullable=False)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey("users.id"), comment="Идентификатор пользователя", nullable=False)
+    job_id = sa.Column(sa.Integer, sa.ForeignKey("jobs.id"), comment="Идентификатор вакансии", nullable=False)
     message = sa.Column(sa.String(500), comment="Сопроводительное письмо")
 
     user = relationship("User", back_populates="responses")
