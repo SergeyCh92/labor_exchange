e63274633942172fb3eef5c8522d7b5e44289130
Chaban Sergey <loriston@rambler.ru>
2024-01-24 12:06:28 +0300

    use allow_population_by_field_name

--- src/schemas/base.py
+++ src/schemas/base.py
@@ -13,4 +13,5 @@ def to_lower_camel_case(snake_str):
 class BaseSchema(BaseModel):
     class Config:
         alias_generator = to_lower_camel_case
+        allow_population_by_field_name = True
         orm_mode = True
